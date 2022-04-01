79993a3ff7687ea5a45e4ceabeef6433569c2e3a
Andrey Popp <8mayday@gmail.com>
2022-04-01 14:58:12 +0300

    Array: add sum and max

--- Array.ts
+++ Array.ts
@@ -1,3 +1,15 @@
 export function* reversed<V>(it: readonly V[]): Iterable<V> {
   for (let i = it.length - 1; i >= 0; i--) yield it[i];
 }
+
+export function sum(it: number[]): number {
+  let acc = 0;
+  for (let v of it) acc += v;
+  return acc;
+}
+
+export function max(it: number[]): number {
+  let acc = -Infinity;
+  for (let v of it) acc = Math.max(acc, v);
+  return acc;
+}
